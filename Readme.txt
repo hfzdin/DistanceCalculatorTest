e1e502d4f6de74181ba74bc23b1de672541a68bf
norhafizudddin <norhafizuddin@commercedc.com.my>
2023-07-27 12:43:57 +0800

    -Spring REST API setup done -set url for the API -error messages if url incorrect

---
+++ Readme.txt
@@ -0,0 +1,40 @@
+kim.sing@woodwing.recruitee.com
+kimsing.kor@woodwing.com
+
+
+1. Identify the requirement -
+
+Input
+-two numbers
+-two units
+-1 unit for output
+
+Output
+-1 numbers with output
+
+web service
+
+Spring Boot REST API,Spring HATEOAS dependency, JDK 17,Maven
+
+2. Create a repo in github
+
+https://github.com/hfzdin/DistanceCalculatorTest
+
+3. Create Spring Boot Rest barebone
+
+-New Spring Starter Project in Eclipse IDE using Spring Rest API,Spring HATEOAS dependecy
+
+4. Write code to accept the three input and the output
+
+-create a controller for the API
+-the url needs to accept 5 inputs
+-test the url validity
+-create error message for exception
+
+
+
+5. Test the code using Rest
+
+
+7. 
+
--- src/main/java/com/example/distanceCalculator/DistanceCalculatorController.java
+++ src/main/java/com/example/distanceCalculator/DistanceCalculatorController.java
@@ -1,5 +1,38 @@
 package com.example.distanceCalculator;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import javax.script.ScriptException;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DistanceCalculatorController {
 
+	 @GetMapping("/")
+	    public String showHomePage() {
+	        return "Please insert according to url /calculate/{value1}/{unit1}/{value2}/{unit2}/{unit3}";
+	    }
+	
+	@RequestMapping("/calculate/{value1}/{unit1}/{value2}/{unit2}/{unit3}")
+	public ResponseEntity calculateValue(
+			@PathVariable int value1, 
+			@PathVariable String unit1,
+			@PathVariable int value2, 
+			@PathVariable String unit2,
+			@PathVariable String unit3) {
+		
+	
+		return new ResponseEntity<>("Please insert according to url /calculate/{value1}/{unit1}/{value2}/{unit2}/{unit3}",HttpStatus.BAD_REQUEST);
+	}
+	
+	
 }
